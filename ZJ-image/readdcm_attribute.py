c159b11781e5721fd34ebd5ed6eb35854f01b42f
zhaojing <zhaojing@comp.nus.edu.sg>
2016-11-17 14:07:51 +0000

    readcm python

---
+++ ZJ-image/readdcm_attribute.py
@@ -0,0 +1,20 @@
+import dicom
+import os
+import numpy
+from matplotlib import pyplot, cm
+import Tkinter
+
+PathDicom = "./"
+lstFilesDCM = []  # create an empty list
+for dirName, subdirList, fileList in os.walk(PathDicom):
+    for filename in fileList:
+        if ".dcm" in filename.lower():  # check whether the file's DICOM
+            lstFilesDCM.append(os.path.join(dirName,filename))
+
+for i in range(len(lstFilesDCM)):
+    # Get ref file
+    RefDs = dicom.read_file(lstFilesDCM[i])
+    print "filename: ", lstFilesDCM[i]
+    print "View position: ", RefDs.ViewPosition
+    print RefDs.ViewPosition == 'PA'
+    print "\n\n"
