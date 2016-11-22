827c9d6f612f8f7e2ec246d378fa92cddf5e455c
zhaojing <zhaojing@comp.nus.edu.sg>
2016-11-22 22:21:34 +0800

    data iter

--- ZJ-image/readdcm.py
+++ ZJ-image/readdcm.py
@@ -3,34 +3,36 @@ import os
 import numpy
 from matplotlib import pyplot, cm
 import Tkinter
+import sys
 
-PathDicom = "./"
-lstFilesDCM = []  # create an empty list
-for dirName, subdirList, fileList in os.walk(PathDicom):
-    for filename in fileList:
-        if ".dcm" in filename.lower():  # check whether the file's DICOM
-            lstFilesDCM.append(os.path.join(dirName,filename))
+# PathDicom = "./"
+# lstFilesDCM = []  # create an empty list
+# for dirName, subdirList, fileList in os.walk(PathDicom):
+#    for filename in fileList:
+#        if ".dcm" in filename.lower():  # check whether the file's DICOM
+#            lstFilesDCM.append(os.path.join(dirName,filename))
 
 # Get ref file
-RefDs = dicom.read_file("./dcm_subimage/I00000009316.dcm")
+RefDs = dicom.read_file(sys.argv[1])
 
 f1 = open('testfile', 'w+')
 
 print >> f1, "RefDs: \n", RefDs
 # Load dimensions based on the number of rows, columns, and slices (along the Z axis)
-ConstPixelDims = (int(RefDs.Rows), int(RefDs.Columns), len(lstFilesDCM))
+ConstPixelDims = (int(RefDs.Rows), int(RefDs.Columns))
 print "\n !!!!!!!!!!!!should write ouput to file!!!!!!!!!!\n"
 # Load spacing values (in mm)
-#ConstPixelSpacing = (float(RefDs.PixelSpacing[0]), float(RefDs.PixelSpacing[1]), float(RefDs.SliceThickness))
+ConstPixelSpacing = (float(RefDs.ImagerPixelSpacing[0]), float(RefDs.ImagerPixelSpacing[1]))
 
-#x = numpy.arange(0.0, (ConstPixelDims[0]+1)*ConstPixelSpacing[0], ConstPixelSpacing[0])
-#y = numpy.arange(0.0, (ConstPixelDims[1]+1)*ConstPixelSpacing[1], ConstPixelSpacing[1])
-#z = numpy.arange(0.0, (ConstPixelDims[2]+1)*ConstPixelSpacing[2], ConstPixelSpacing[2])
-
+x = numpy.arange(0.0, (ConstPixelDims[0]+1)*ConstPixelSpacing[0], ConstPixelSpacing[0])
+y = numpy.arange(0.0, (ConstPixelDims[1]+1)*ConstPixelSpacing[1], ConstPixelSpacing[1])
+# z = numpy.arange(0.0, (ConstPixelDims[2]+1)*ConstPixelSpacing[2], ConstPixelSpacing[2])
+print "x = \n", x
+print "y = \n", y
 # The array is sized based on 'ConstPixelDims'
 ArrayDicom = numpy.zeros(ConstPixelDims, dtype=RefDs.pixel_array.dtype)
 
-ArrayDicom[:, :, 0] = RefDs.pixel_array
+ArrayDicom[:, :] = RefDs.pixel_array
 print "RefDs.pixel_array shape: \n", RefDs.pixel_array.shape
 # loop through all the DICOM files
 #for filenameDCM in lstFilesDCM:
@@ -44,4 +46,5 @@ print "RefDs.pixel_array shape: \n", RefDs.pixel_array.shape
 pyplot.figure(dpi=300)
 pyplot.axes().set_aspect('equal', 'datalim')
 pyplot.set_cmap(pyplot.gray())
-pyplot.pcolormesh(x, y, numpy.flipud(ArrayDicom[:, :, 80]))
+pyplot.pcolormesh(x, y, numpy.flipud(ArrayDicom[:, :]))
+pyplot.show()
